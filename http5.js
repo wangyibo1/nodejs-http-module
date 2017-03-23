ad750615f39a51abe044b348986a1b6746b7f8f7
wangyibo1 <yibowang0@gmail.com>
2017-03-23 20:35:26 +0800

    1

---
+++ http5.js
@@ -0,0 +1,39 @@
+/**
+ * Created by YiboWang on 2017/3/23.
+ */
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var ROOT_DIR = "html/";
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true, false);
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(8080);
+
+
+
+var options = {
+    hostname: 'localhost',
+    port: '8080',
+    path: '/hello.html'
+};
+function handleResponse(response) {
+    var serverData = '';
+    response.on('data', function (chunk) {
+        serverData += chunk;
+    });
+    response.on('end', function () {
+        console.log(serverData);
+    });
+}
+http.request(options, function(response){
+    handleResponse(response);
+}).end();
---
+++ http8.js
@@ -0,0 +1,22 @@
+/**
+ * Created by YiboWang on 2017/3/23.
+ */
+var http = require('http');
+var options = {
+    hostname: 'localhost',
+    port: '8080',
+};
+function handleResponse(response) {
+    var serverData = '';
+    response.on('data', function (chunk) {
+        serverData += chunk;
+    });
+    response.on('end', function () {
+        console.log("Response Status:", response.statusCode);
+        console.log("Response Headers:", response.headers);
+        console.log(serverData);
+    });
+}
+http.request(options, function(response){
+    handleResponse(response);
+}).end();
